8a9d23b8c9127412b68deb640df3321f974ed30d
Anthony <AnthonyArzola@gmail.com>
2020-04-12 11:58:27 -0700

    Remove unnecessary inout modifier.

--- Algorithms/Searching/BinarySearch.playground/Contents.swift
+++ Algorithms/Searching/BinarySearch.playground/Contents.swift
@@ -2,12 +2,11 @@
 func binarySearch(array: [Int], value: Int) -> Int {
     guard !array.isEmpty else { return -1 }
     
-    var arrayCopy = array
     print("Search for \(value) within \(array)")
-    return recursiveSearch(array: &arrayCopy, leftIndex: 0, rightIndex: array.count - 1, value: value)
+    return recursiveSearch(array: array, leftIndex: 0, rightIndex: array.count - 1, value: value)
 }
 
-func recursiveSearch(array: inout [Int], leftIndex: Int, rightIndex: Int, value: Int) -> Int {
+func recursiveSearch(array: [Int], leftIndex: Int, rightIndex: Int, value: Int) -> Int {
     print("Left index: \(leftIndex), right index: \(rightIndex)")
     if leftIndex <= rightIndex {
         // Determine array mid-point
@@ -20,11 +19,11 @@ func recursiveSearch(array: inout [Int], leftIndex: Int, rightIndex: Int, value:
         } else if value > array[middleIndex] {
             print("No match found. Value \(value) is > midpoint value \(array[middleIndex]). Search to the right.")
             // Continue searching right-hand-side (exclude index value checked already)
-            return recursiveSearch(array: &array, leftIndex: middleIndex + 1, rightIndex: rightIndex, value: value)
+            return recursiveSearch(array: array, leftIndex: middleIndex + 1, rightIndex: rightIndex, value: value)
         } else {
             print("No match found. Value \(value) is < midpoint value \(array[middleIndex]). Search to the left.")
             // Continue searching left-hand-side (exclude index value checked already)
-            return recursiveSearch(array: &array, leftIndex: leftIndex, rightIndex: middleIndex - 1, value: value)
+            return recursiveSearch(array: array, leftIndex: leftIndex, rightIndex: middleIndex - 1, value: value)
         }
     }
     print("Left index \(leftIndex) >= \(rightIndex) right index. Return -1 since we couldn't find a match.")
@@ -36,7 +35,7 @@ var numbers = [7, 21, 50, 51, 55, 56, 83, 500, 1003, 50000, 2000048]
 numbers.sort()
 
 var index = binarySearch(array: numbers, value: 1003)
-print("Searching for 1003 found at index \(index)")
+print("Searching for 1003 found at index \(index)\n")
 
 index = binarySearch(array: numbers, value: 22)
-print("Searching for 22 found at index \(index)")
+print("Searching for 22 found at index \(index)\n")
