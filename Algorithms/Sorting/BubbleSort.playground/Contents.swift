b8dbb964db5623cb3e600783a07c811745424289
Anthony <AnthonyArzola@gmail.com>
2020-04-12 11:55:55 -0700

    Add print statements in order to help understand how search is performed at each interval.

--- Algorithms/Sorting/BubbleSort.playground/Contents.swift
+++ Algorithms/Sorting/BubbleSort.playground/Contents.swift
@@ -1,6 +1,6 @@
 /// Perform in-place sort
 func bubbleSort(array: inout [Int]) {
-    
+
     var continueChecking = true
     while continueChecking {
         var swaps = 0
@@ -8,10 +8,12 @@ func bubbleSort(array: inout [Int]) {
         for index in 0..<array.count - 1 {
             if array[index] > array[index + 1] {
                 // Current element evaluated is greater than next value...swap them
+                print("\(array[index]) is bigger than right neighbor \(array[index + 1]). Bubble it up! ")
                 let elementValue = array[index]
                 array[index] = array[index + 1]
                 array[index + 1] = elementValue
                 swaps += 1
+                print("Array after swap: \(array)\n")
             }
         }
         continueChecking = swaps > 0
@@ -19,5 +21,6 @@ func bubbleSort(array: inout [Int]) {
 }
 
 var array = [3, 50, 1000, 88, 77, 4, 1]
+print("Array before sorting: \(array)\n")
 bubbleSort(array: &array)
-print("array after soring: \(array)")
+print("Array after soring: \(array)")
