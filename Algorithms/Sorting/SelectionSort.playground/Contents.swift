c9101cc320b1a520ff0d83800d11d03fa21d09a1
Anthony <AnthonyArzola@gmail.com>
2020-05-09 22:17:40 -0700

    Add comments to Selection Sort implementation

--- Algorithms/Sorting/SelectionSort.playground/Contents.swift
+++ Algorithms/Sorting/SelectionSort.playground/Contents.swift
@@ -2,17 +2,21 @@
 func selectionSort(array: inout [Int]) {
     guard !array.isEmpty else { return }
     
+    // Process input array once
     for (unsortedIndex, element) in array.enumerated() {
         var smallestValueIndex = unsortedIndex
         var index = unsortedIndex
         
+        // Find smallest value within unsorted section
         while index < array.count {
             if array[index] <= array[smallestValueIndex] {
+                // Found smaller value
                 smallestValueIndex = index
             }
             index += 1
         }
-        
+
+        // Swap smallest value with last known unsorted index
         let temp = array[unsortedIndex]
         array[unsortedIndex] = array[smallestValueIndex]
         array[smallestValueIndex] = temp
