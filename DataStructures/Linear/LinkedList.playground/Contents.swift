8da02f39cb6992a4481145f6fa0b26df11d76f70
Anthony Arzola <AnthonyArzola@gmail.com>
2024-02-20 23:33:27 -0800

    Update Linked LIst implementation

--- DataStructures/Linear/LinkedList.playground/Contents.swift
+++ DataStructures/Linear/LinkedList.playground/Contents.swift
@@ -1,30 +1,37 @@
 import UIKit
 
-class LinkedNode<T> {
+class LinkedNode<T: Comparable> {
     init(value: T, nextNode: LinkedNode<T>? = nil) {
         self.value = value
         self.nextNode = nextNode
     }
-    // Stored value
+    // Stored value.
     let value: T
     // Pointer to next linked node.
     var nextNode: LinkedNode<T>?
 }
 
+extension LinkedNode: Equatable {
+    static func == (lhs: LinkedNode<T>, rhs: LinkedNode<T>) -> Bool {
+        return lhs.value == rhs.value
+        && lhs.nextNode == rhs.nextNode
+    }
+}
+
 /*
  Class that implements link-list data structure
  Ex:    [3]->nil (single node where first/last are the same)
-        [23]->[3]->nil (two nodes, where first node is 3, last node is 23)
+        [55]->[3]->nil (two nodes, where first node is 55, last node is 3)
  */
 
-class LinkedList<T> {
+class LinkedList<T: Comparable> {
     var firstNode: LinkedNode<T>?
     var lastNode: LinkedNode<T>?
     private(set) var count: Int = 0
     
     /// Add new `LinkedNode` to front.
-    /// Ex: Existing LinkedList [3]->nil with call to `addFirst(35)` will
-    /// result in [3]->[35]->nil where `firstNode` is 35 and `lastNode` is 3.
+    /// Ex: Existing LinkedList [3]->nil with call to `addFirst(55)` will
+    /// result in [55]->[3]->nil where `firstNode` is 55 and `lastNode` is 3.
     func addFirst(value: T) {
         guard count > 0 else {
             // LinkedList is empty, make it first and last node.
@@ -35,19 +42,19 @@ class LinkedList<T> {
         }
         
         if let first = firstNode {
-            let previousNode = first
+            let previousFirstNode = first
             let newFirstNode = LinkedNode(value: value)
-            previousNode.nextNode = newFirstNode
             
             // Update pointers, count
+            newFirstNode.nextNode = previousFirstNode
             firstNode = newFirstNode
             count += 1
         }
     }
 
     /// Add new `LinkedNode` at the end
-    /// Ex: Existing LinkedList [3]->nil with call to `addLast(23)` will
-    /// result in [23]->[3]->nil where `firstNode` is 3 and `lastNode` is 23.
+    /// Ex: Existing LinkedList [3]->nil with call to `addLast(45)` will
+    /// result in [3]->[45]->nil where `firstNode` is 3 and `lastNode` is 45.
     func addLast(value: T) {
         guard count > 0 else {
             // LinkedList is empty, make new last node equal to first node.
@@ -57,16 +64,54 @@ class LinkedList<T> {
             return
         }
         if let last = lastNode {
-            let previous = lastNode
+            let previousLastNode = last
             let newLastNode = LinkedNode(value: value)
             
             // Update pointers, count
-            newLastNode.nextNode = previous
+            previousLastNode.nextNode = newLastNode
             lastNode = newLastNode
             count += 1
         }
     }
     
+    /// Remove first `LinkedNode`.
+    func removeFirst() {
+        guard count > 0 else {
+            return
+        }
+        if let first = firstNode {
+            firstNode = firstNode?.nextNode
+            count -= 1
+        }
+    }
+    
+    /// Remove last `LinkedNode`.
+    func removeLast() {
+        guard count > 0 else {
+            return
+        }
+        var currentNode = firstNode
+        for index in 0..<count {
+            currentNode = currentNode?.nextNode
+        }
+        currentNode?.nextNode = nil
+        lastNode = currentNode
+        count -= 1
+    }
+    
+    /// Search Linked LIst for specified value.
+    func contains(value: T) -> Bool {
+        var currentNode = firstNode
+        while currentNode != nil {
+            if currentNode?.value == value {
+                return true
+            }
+            currentNode = currentNode?.nextNode
+        }
+        return false
+    }
+    
+    /// Clear Linked List.
     func clear() {
         firstNode = nil
         lastNode = nil
@@ -85,18 +130,41 @@ class LinkedList<T> {
 }
 
 var linkedList = LinkedList<Int>()
+// Add first
 linkedList.addFirst(value: 4)
 assert(linkedList.count == 1)
 assert(linkedList.firstNode?.value ?? Int.min == 4)
 
+// Add first
 linkedList.addFirst(value: 32)
 assert(linkedList.count == 2)
 assert(linkedList.firstNode?.value ?? Int.min == 32)
 assert(linkedList.lastNode?.value ?? Int.min == 4)
 
+// Add last
 linkedList.addLast(value: 5)
 assert(linkedList.count == 3)
 assert(linkedList.lastNode?.value ?? Int.min == 5)
 
+//Search
+linkedList.printNodes()
+assert(linkedList.contains(value: 5))
+assert(!linkedList.contains(value: 55))
+
+// Clear
 linkedList.clear()
 assert(linkedList.count == 0)
+assert(!linkedList.contains(value: 5))
+
+// Add last, remove first
+linkedList.addLast(value: 28)
+assert(linkedList.count == 1)
+linkedList.removeLast()
+assert(linkedList.count == 0)
+
+// Add last, add last, remove first
+linkedList.addLast(value: 11)
+linkedList.addLast(value: 22)
+linkedList.removeFirst()
+assert(linkedList.count == 1)
+assert(linkedList.firstNode?.value == 22)
